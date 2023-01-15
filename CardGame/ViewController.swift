9742cd03fff6f41083660eee9c913f9412dbcec5
birjuKr <birju@roam.ai>
2023-01-15 16:24:13 +0530

    Initial Commit

---
+++ CardGame/ViewController.swift
@@ -0,0 +1,60 @@
+//
+//  ViewController.swift
+//  CardGame
+//
+//  Created by Bk on 14/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var leftImageView: UIImageView!
+    
+    @IBOutlet weak var rightImageView: UIImageView!
+    
+    @IBOutlet weak var leftScoreLabel: UILabel!
+    
+    @IBOutlet weak var rightScoreLabel: UILabel!
+    
+    var leftScore = 0
+    var rightScore = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func dealTapped(_ sender: Any) {
+        
+        // generating random number
+        let leftNumber = Int.random(in: 2...14)
+        
+        let rightNumber = Int.random(in: 2...14)
+        
+        // updating the imageview
+        leftImageView.image = UIImage(named: "card\(leftNumber)")
+        
+        rightImageView.image = UIImage(named: "card\(rightNumber)")
+        
+        if leftNumber > rightNumber {
+            
+            //left side win
+            leftScore += 1
+            leftScoreLabel.text = String(leftScore)
+            
+        }else if leftNumber < rightNumber {
+            
+            //right side win
+            rightScore += 1
+            rightScoreLabel.text = String(rightScore)
+        }else{
+            
+            //tie
+        }
+    }
+    
+}
+
